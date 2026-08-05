7ce1b237089c3d07a85a7140c8f63feb18b3013d
SemenovaC <christina.semenova@mail.ru>
2026-08-05 21:05:21 +0300

    Start project

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -1,4 +1,29 @@
 package ui_tests;
 
-public class RegistrationTests {
+import dto.UserLombok;
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.Random;
+
+public class RegistrationTests extends AppManager {
+    @BeforeMethod
+    public void goToRegistrationLoginPage(){
+        new HomePage(getDriver()).clickBtnLogin();
+    }
+
+    @Test
+    public void registrationPositiveTest(){
+        int i = new Random().nextInt(1000);
+        UserLombok user = UserLombok.builder()
+                .username("bruno"+i+"@gmail.com")
+                .password("QAZ123!lnk")
+                .build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm(user);
+        loginPage.clickBtnRegistration();
+    }
 }
