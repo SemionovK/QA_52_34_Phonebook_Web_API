1d93731334e81ce32f5fef1e01ddd103317a1f49
SemenovaC <christina.semenova@mail.ru>
2026-09-05 15:59:23 +0300

    Listeners added

--- src/test/java/ui_tests/RegistrationTests.java
+++ src/test/java/ui_tests/RegistrationTests.java
@@ -5,13 +5,17 @@ import dto.UserLombok;
 import manager.AppManager;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
-import static utils.UserFactory.*;
+import utils.TestNGListener;
 
+import static utils.UserFactory.*;
 import java.util.Random;
+
+@Listeners(TestNGListener.class)
 
 public class RegistrationTests extends AppManager {
     LoginPage loginPage;
